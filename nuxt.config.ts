74f14976de697f68412eeac499ac0ee7108d1f70
kaceo <kaceong@gmail.com>
2024-05-21 11:52:41 +0800

    clean

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,5 +100,19 @@ export default defineNuxtConfig({
   },
   colorMode: {preference: 'dark',},
 
+  hooks: {                                                                           // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c =>
+        ['UButton', 'UIcon'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  __routeRules: {
+    '/api/search.json': { prerender: true }
+  },
 
 })
